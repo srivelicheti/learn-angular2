aac1a69c48f2e7e78a928146e38479d5e64148a5
Sri Velicheti <velicheti.harsha@gmail.com>
2016-09-29 22:15:46 -0400

    latest

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "angular2/core"
-import{IProduct} from "./product"
+import {IProduct} from "./product"
 import {StarComponent} from '../shared/star.component'
+import {ProductService} from './product.service'
 
 @Component({
     selector:'pm-products',
@@ -11,35 +12,26 @@ import {StarComponent} from '../shared/star.component'
 })
 
 export class ProductListComponent implements OnInit{
+    private _productService : ProductService;
+    constructor(prodService : ProductService)  {
+        this._productService = prodService;
+        //this.products = prodService.getProducts();
+    }
+    // productService : ProductService = 
     pageTitle: string ="Product List";
     imageWidth: number = 50;
     imageMargin:number = 2;
     showImage:boolean= false;
     filterString:string ='cart';
-    products:IProduct[] = [{
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }];
+    products:IProduct[] ;
     toggleImage(): void {
         this.showImage = !this.showImage;
     };
     ngOnInit():void {
-        console.log('In OnInit');
+        // console.log('In OnInit');
+        this.products = this._productService.getProducts();
+    };
+    onRatingClicked(event:string ) : void{
+        console.log(event);
     }
 }
--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,15 +1,23 @@
-import {Component , OnInit, Input } from 'angular2/core'
+import {Component , OnInit, Input, Output, OnChanges,EventEmitter } from 'angular2/core'
 
 @Component({
     selector:'aci-star',
     templateUrl:'app/shared/star.component.html',
     styleUrls:['app/shared/star.component.css']
 })
-export class StarComponent implements OnInit {
+export class StarComponent implements OnInit, OnChanges {
     @Input('rating')
     rating : number;
     starWidth : number;
     ngOnInit() : void{
         this.starWidth = (this.rating * 86)/5;
+    };
+    ngOnChanges() :void{
+        this.starWidth = (this.rating * 86)/5;
+    }
+    @Output()
+    ratingClicked : EventEmitter<string> = new EventEmitter<string>();
+    onRatingClick() : void{
+        this.ratingClicked.emit('clicked!!');
     }
 }
